41bbbaef07f45130bca1cf124b403985a6940ec9
MichelDierckx <michel.dierckx@hotmail.com>
2024-11-27 16:45:04 +0100

    added document id's to embedding

--- src/ranker/ranker.py
+++ src/ranker/ranker.py
@@ -48,11 +48,23 @@ class VectorStore:
         )
 
 
+def _extract_id_from_filename(filename: str) -> int:
+    """
+    Extract textfile id from filename.
+    :param filename: the filename (not including the path)
+    :return: the integer present in the filename.
+    """
+    id_str = filename.split('_')[1]
+    id_str = id_str.split('.')[0]
+    return int(id_str)
+
+
 def _index_documents(documents_dir: str, work_dir: str):
     # Load a pretrained Sentence Transformer model
     model_name = "all-MiniLM-L6-v2"
     model = SentenceTransformer(model_name)
-    logger.debug(f"Loaded model '{model_name}' (max sequence length = {model.max_seq_length})")
+    logger.debug(
+        f"Loaded model '{model_name}' (max sequence length = {model.max_seq_length}, embedding dimension = {model.get_sentence_embedding_dimension()})")
 
     # List documents
     text_files = [f for f in os.listdir(documents_dir) if
@@ -67,12 +79,17 @@ def _index_documents(documents_dir: str, work_dir: str):
     batch_size = 10_000
     for i in tqdm(range(0, len(text_files), batch_size), desc="Generating embeddings", unit=f"{batch_size} documents"):
         documents = []
+        document_ids = []
         for text_file in text_files[i:i + batch_size]:
+            document_ids.append(_extract_id_from_filename(text_file))
             full_path = os.path.join(documents_dir, text_file)
             with open(full_path, "r", encoding='utf-8') as f:
                 documents.append(f.read())
         embeddings = model.encode(documents, batch_size=32, show_progress_bar=True, convert_to_numpy=True,
                                   precision="float32", normalize_embeddings=True)
         df = pd.DataFrame(embeddings)
+        df.columns = df.columns.map(str)
+        df["id"] = document_ids
+        print(df)
         vector_store.write(df)
     vector_store.cleanup()
