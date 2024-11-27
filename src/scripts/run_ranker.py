76b7611da020710947792cbf8c1c82b1ba0a0cee
MichelDierckx <michel.dierckx@hotmail.com>
2024-11-27 15:20:44 +0100

    removed line for testing

--- src/scripts/run_ranker.py
+++ src/scripts/run_ranker.py
@@ -1,13 +1,10 @@
 from typing import Union, List
 
-import lance
-
 from src.ranker.config import Config
 from src.utils.logger_setup import setup_logging
 
 
 def main(args: Union[str, List[str]] = None) -> int:
-    ds = lance.dataset("fake")
     setup_logging()
     config = Config()
     config.parse(args)
