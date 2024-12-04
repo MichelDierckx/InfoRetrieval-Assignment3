eb346a61ac41a643a6087ae94b98b00f14c321cf
MichelDierckx <michel.dierckx@hotmail.com>
2024-12-04 17:28:40 +0100

    better status updating

--- src/ranker/ranker.py
+++ src/ranker/ranker.py
@@ -1,4 +1,5 @@
 import os
+import time
 from typing import Tuple
 
 import faiss
@@ -40,10 +41,18 @@ def _rank_documents(query_file: str, index_file: str, rankings_filename: str, wo
     rankings_filepath = os.path.join(work_dir, rankings_filename + '.csv')
     df_headers.to_csv(rankings_filepath, index=False)
 
+    query_count = 0
+    query_time_start = time.time()
+
+    # (wrap generator in a tqdm object for status updates)
+    batches = tqdm(pd.read_csv(query_file, chunksize=BATCH_SIZE), desc="Processing queries",
+                   unit=f"{BATCH_SIZE} queries", bar_format='{desc}')
+
     # Read queries in batch, convert to embeddings and query against the index
-    for batch in tqdm(pd.read_csv(query_file, chunksize=BATCH_SIZE)):
+    for batch in batches:
         # read queries
         queries = batch["Query"].astype(str).tolist()
+        query_count += len(queries)
         # generate embeddings for queries
         embeddings = model.encode(queries, batch_size=32, show_progress_bar=False, convert_to_numpy=True,
                                   precision="float32", normalize_embeddings=True)
@@ -54,11 +63,17 @@ def _rank_documents(query_file: str, index_file: str, rankings_filename: str, wo
         ids = batch["Query number"].to_numpy()
         ids_repeated = np.repeat(ids, nn_matrix.shape[1])
         nn_flat = nn_matrix.flatten()
-
         df = pd.DataFrame({"Query_number": ids_repeated, "doc_number": nn_flat})
 
-        # Append DataFrame to the CSV file
+        # append results to output file
         df.to_csv(rankings_filepath, mode='a', index=False, header=False)
+
+        # update status message
+        batches.set_description_str(f"Processed {query_count} queries.")
+        batches.update(query_count)
+
+    logger.info(f"Processed {query_count} queries in {time.time() - query_time_start} seconds.")
+    logger.info(f"Query results have been written to '{rankings_filepath}'.")
 
 
 def _query(index: faiss.Index, query_embeddings: npt.NDArray[np.float32]) -> Tuple[
