682bf75b5031473041d3d3435bf29de4a34b21b1
MichelDierckx <michel.dierckx@hotmail.com>
2024-12-07 17:54:09 +0100

    some cluster tuning bugfixes and optimizations + kneedle algorithm to find elbow point

--- src/clustering_tuner/clustering_tuner.py
+++ src/clustering_tuner/clustering_tuner.py
@@ -1,8 +1,10 @@
 import math
 import os
 import time
+from typing import List
 
 import faiss
+import kneed
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -43,7 +45,7 @@ def _elbow_method(embeddings_store: EmbeddingsStore, embeddings_dir: str, work_d
     # nlist_min = math.ceil(1 * math.sqrt(nr_embeddings)
     nlist_min = 100
     # at least 39*nr_clusters training points required, we take 50 as minimum to be safe
-    nlist_max = min(math.ceil(7 * math.sqrt(nr_embeddings)), nr_embeddings // 50)
+    nlist_max = min(math.ceil(18 * math.sqrt(nr_embeddings)), nr_embeddings // 39)
 
     # distance metric (equal to cosine similarity if embeddings are normalized)
     metric = faiss.METRIC_INNER_PRODUCT
@@ -58,7 +60,7 @@ def _elbow_method(embeddings_store: EmbeddingsStore, embeddings_dir: str, work_d
         f"Initiating elbow method analysis: testing cluster counts from {nlist_min} to {nlist_max} with a step size of {step_size}.")
 
     # sample_size should at least be 39*nr_clusters
-    sample_size = min(50 * nlist_max, nr_embeddings)
+    sample_size = min(39 * nlist_max, nr_embeddings)
     # retrieve samples from embeddings storage
     samples = embeddings_store.sample(sample_size)
 
@@ -74,21 +76,28 @@ def _elbow_method(embeddings_store: EmbeddingsStore, embeddings_dir: str, work_d
         centroids = index.quantizer.reconstruct_n(0, index.nlist)
         index.add(centroids)
 
-        start_time = time.time()
+        start_time = time.perf_counter()
         # retrieve distances for training samples to every centroid
         D, I = index.search(samples, 1)
-        search_time = time.time() - start_time
+        search_time_micros = (time.perf_counter() - start_time) * 1_000_000
+
+        # D contains cosine similarity, convert to cosine distance
+        D = 1.0 - D
 
         # https://www.geeksforgeeks.org/elbow-method-for-optimal-value-of-k-in-kmeans/
         average_distances_to_centroid = np.mean(D)
         # average search time in ms
-        average_search_time = int((search_time / sample_size) * 1000)
+        average_search_time = (search_time_micros / sample_size)
 
         # save results
         nlists.append(nlist)
         sample_sizes.append(sample_size)
         average_distances_results.append(average_distances_to_centroid)
         average_search_time_results.append(average_search_time)
+
+    # use kneedle algorithm to find elbow point
+    elbow_point = _find_elbow_point(nlists=nlists, average_distances=average_distances_results)
+    logger.info(f"Optimal number of clusters according to elbow method = {elbow_point}.")
 
     logger.info(f"Finished elbow analysis in {elapsed_time_to_string(time.time() - start_testing_time)}")
     # write results to disk
@@ -97,12 +106,12 @@ def _elbow_method(embeddings_store: EmbeddingsStore, embeddings_dir: str, work_d
                    average_search_time_results=average_search_time_results, embeddings_name=embeddings_name,
                    work_dir=work_dir)
     # plot results
-    _plot_results(nlists=nlists, average_distances=average_distances_results,
+    _plot_results(elbow_point=elbow_point, nlists=nlists, average_distances=average_distances_results,
                   average_search_time_results=average_search_time_results,
                   embeddings_name=embeddings_name, work_dir=work_dir)
 
 
-def _write_results(nlists: [], sample_sizes: [], average_distances: [], average_search_time_results: [],
+def _write_results(nlists: List, sample_sizes: List, average_distances: List, average_search_time_results: List,
                    embeddings_name: str,
                    work_dir: str):
     output_file = os.path.join(work_dir, f"{embeddings_name}_elbow_method_results.csv")
@@ -117,7 +126,8 @@ def _write_results(nlists: [], sample_sizes: [], average_distances: [], average_
     logger.info(f"Saved elbow method statistics to '{output_file}'.")
 
 
-def _plot_results(nlists: [], average_distances: [], average_search_time_results: [], embeddings_name: str,
+def _plot_results(elbow_point: int, nlists: List, average_distances: List, average_search_time_results: List,
+                  embeddings_name: str,
                   work_dir: str):
     # plot distortion
     output_file_distortion = os.path.join(work_dir, f"{embeddings_name}_distortion.png")
@@ -126,6 +136,7 @@ def _plot_results(nlists: [], average_distances: [], average_search_time_results
     plt.ylabel('Distortion')
     plt.title('The Elbow Method using Distortion')
     plt.grid()
+    plt.vlines(elbow_point, plt.ylim()[0], plt.ylim()[1], linestyles='dashed')
     plt.savefig(output_file_distortion)
     logger.info(f"Saved distortion graph  to '{output_file_distortion}'.")
     plt.close()
@@ -134,9 +145,27 @@ def _plot_results(nlists: [], average_distances: [], average_search_time_results
     output_file_average_search_time = os.path.join(work_dir, f"{embeddings_name}_search_time.png")
     plt.plot(nlists, average_search_time_results, 'bx-')
     plt.xlabel('Number of Clusters (k)')
-    plt.ylabel('Average search time (ms) per query')
+    plt.ylabel('Average search time (microsecond) for a cluster per query')
     plt.title('Search Time vs Number of Clusters')
     plt.grid()
     plt.savefig(output_file_average_search_time)
     logger.info(f"Saved search time graph  to '{output_file_average_search_time}'.")
     plt.close()
+
+
+def _find_elbow_point(nlists: List, average_distances: List):
+    """
+    Determine the elbow point using kneedle algorithm (https://www1.icsi.berkeley.edu/~barath/papers/kneedle-simplex11.pdf)
+    :param nlists: number of cluster values (x)
+    :param average_distances: average distances to nearest centroids (y)
+    :return: elbow point (optimal number of clusters according to elbow method)
+    """
+    kn = kneed.KneeLocator(
+        nlists,
+        average_distances,
+        curve='convex',
+        direction='decreasing',
+        interp_method='interp1d',
+    )
+    knee_point_x = kn.knee
+    return knee_point_x
