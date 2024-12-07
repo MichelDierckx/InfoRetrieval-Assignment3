e10b4719d34cf7049b08eb8d1c3b77c62180ca88
MichelDierckx <michel.dierckx@hotmail.com>
2024-12-07 21:59:44 +0100

    nprobes search tuner

---
+++ src/search_tuner/config.py
@@ -0,0 +1,217 @@
+import os
+from typing import Optional, Any
+
+import configargparse
+
+from src.utils.logger_setup import get_logger
+
+# Create a logger for this module
+logger = get_logger(__name__)
+
+
+class Config:
+    def __init__(self):
+        self._parser = configargparse.ArgParser(
+            description="search tuner: find an appropriate value for the faiss nprobes variable for a given set of queries, a ground truth and an IVF index.",
+            default_config_files=["config_nprobes.ini"],
+            args_for_setting_config_path=["-c", "--config"],
+            formatter_class=configargparse.ArgumentDefaultsHelpFormatter,
+        )
+        self._define_arguments()
+        self._namespace = None
+
+    def get(self, option: str, default: Optional[Any] = None) -> Optional[Any]:
+        """
+        Retrieve configuration options with a default value if the option does not exist.
+        :param option: str, the configuration option to retrieve.
+        :param default: Optional[str], the default value to return if the option does not exist. Defaults to None.
+        :return: Optional[str], The value of the configuration option or the default value.
+        """
+        if self._namespace is None:
+            raise RuntimeError("The configuration has not been initialized. Call `parse()` first.")
+        return self._namespace.get(option, default)
+
+    def parse(self, args_str: Optional[str] = None) -> None:
+        """
+        Parse the configuration settings.
+
+        Parameters
+        ----------
+        args_str : Optional[str]
+            If None, arguments are taken from sys.argv; otherwise, a string of arguments.
+            Arguments not specified on the command line are taken from the config file.
+        """
+        if self._namespace is not None:
+            return  # Skip parsing if already parsed
+
+        # Parse the arguments
+        self._namespace = vars(self._parser.parse_args(args_str))
+        self._validate_file_path("index", [".index"])
+        self._validate_file_path("queries", [".csv", ".tsv"])
+        self._validate_directory_path("work_dir")
+        self._validate_file_path("gt", [".csv", ".tsv"])
+        self._validate_positive_int("min_nprobes", True)
+        self._validate_positive_int("max_nprobes", True)
+        self._validate_positive_int("step_size", True)
+        self._log_parameters()
+
+    def _define_arguments(self) -> None:
+        """
+        Define the command-line and config file arguments.
+        """
+        # IO arguments
+        self._parser.add_argument(
+            "--queries",
+            required=True,
+            help=(
+                "File containing the queries (Supports .csv and .tsv)"
+            ),
+            type=str,
+            action='store',
+            dest="queries",
+            metavar="<path>",
+        )
+
+        self._parser.add_argument(
+            "--index",
+            required=True,
+            help=(
+                "File containing a FAISS index (supports faiss index types 'IndexIDMap(IndexFlatIP)', IndexIVFFlat)"
+            ),
+            type=str,
+            action='store',
+            dest="index",
+            metavar="<path>",
+        )
+
+        self._parser.add_argument(
+            "--work_dir",
+            required=True,
+            help="Working directory, used to write temporary files and output files.",
+            type=str,
+            action='store',
+            dest="work_dir",
+            metavar="<path>",
+        )
+
+        self._parser.add_argument(
+            "--gt",
+            required=True,
+            help=(
+                "File containing the reference ranking used for evaluation. Supports .csv and .tsv"
+            ),
+            type=str,
+            action='store',
+            dest="gt",
+            metavar="<path>",
+        )
+
+        self._parser.add_argument(
+            "--min_nprobes",
+            required=True,
+            help=(
+                "Minimum number of clusters to probe during FAISS nprobes tuning."
+            ),
+            type=int,
+            action='store',
+            dest="min_nprobes",
+        )
+
+        self._parser.add_argument(
+            "--max_nprobes",
+            required=True,
+            help=(
+                "Maximum number of clusters to probe during FAISS nprobes tuning."
+            ),
+            type=int,
+            action='store',
+            dest="max_nprobes",
+        )
+
+        self._parser.add_argument(
+            "--step_size",
+            required=True,
+            help="Specifies the increment for the number of nprobes tested. "
+                 "For example, a step size of 5 will test nprobes in increments of 5 (e.g., 5, 10, 15, ...).",
+            type=int,
+            action='store',
+            dest="step_size",
+            metavar="<int>",
+        )
+
+    def _validate_directory_path(self, param: str, required_extensions: Optional[list] = None) -> None:
+        """
+        Validate that a directory path exists and optionally check if it has one of the required extensions.
+
+        Parameters:
+        ----------
+        param : str
+            The name of the parameter to validate.
+
+        required_extensions : Optional[list], optional
+            A list of file extensions to validate against. If provided, checks that at least one file
+            in the directory matches one of the extensions.
+        """
+        path = self.get(param)
+        if not os.path.isdir(path):  # Check if path is a valid directory
+            raise NotADirectoryError(f"--{param}: Path '{path}' is not a valid directory.")
+
+        if required_extensions:
+            # Check if any file in the directory matches the required extensions
+            files_with_valid_extension = [
+                file for file in os.listdir(path) if any(file.endswith(ext) for ext in required_extensions)
+            ]
+            if not files_with_valid_extension:
+                valid_extensions = ', '.join(required_extensions)
+                raise ValueError(
+                    f"--{param}: No files with extensions {valid_extensions} found in the directory '{path}'.")
+
+    def _validate_file_path(self, param: str, required_extensions: Optional[list] = None) -> None:
+        """
+        Validate that a file path exists and optionally check if it has one of the required extensions.
+
+        Parameters:
+        ----------
+        param : str
+            The name of the parameter to validate.
+
+        required_extensions : Optional[list], optional
+            A list of valid extensions to check the file against. If provided, ensures that the file ends with one of them.
+        """
+        path = self.get(param)
+        if not os.path.isfile(path):  # Check if the path is a valid file
+            raise FileNotFoundError(f"--{param}: Path '{path}' is not a valid file.")
+
+        if required_extensions:
+            if not any(path.endswith(ext) for ext in required_extensions):
+                valid_extensions = ', '.join(required_extensions)
+                raise ValueError(
+                    f"--{param}: Path '{path}' does not end with one of the following extensions: {valid_extensions}"
+                )
+
+    def _validate_positive_int(self, param: str, strict: bool = False) -> None:
+        value = self.get(param)
+        int_value = int(value)
+        if (strict and int_value <= 0) or (not strict and int_value < 0):
+            comparison = "greater than 0" if strict else "0 or greater"
+            raise ValueError(f"--{param}: {value} is not a positive integer ({comparison}).")
+
+    def _log_parameters(self) -> None:
+        """Log all chosen parameters."""
+        for key, value in self._namespace.items():
+            logger.debug(f"{key}: {value}")
+
+    def __getattr__(self, option):
+        """
+        Retrieve configuration options as attributes.
+
+        Raises a KeyError with a helpful message if the option does not exist.
+        """
+        if self._namespace is None:
+            raise RuntimeError("The configuration has not been initialized. Call `parse()` first.")
+        if option not in self._namespace:
+            raise KeyError(f"The configuration option '{option}' does not exist.")
+        return self._namespace[option]
+
+    def __getitem__(self, item):
+        return self.__getattr__(item)
---
+++ src/search_tuner/search_tuner.py
@@ -0,0 +1,188 @@
+import os
+import time
+from typing import Tuple, List
+
+import faiss
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from matplotlib import pyplot as plt
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+
+from src.search_tuner.config import Config
+from src.utils.logger_setup import get_logger
+
+logger = get_logger(__name__)
+
+
+def run(config: Config):
+    _find_optimal_nr_probes(query_file=config.queries, index_file=config.index,
+                            work_dir=config.work_dir, gt=config.gt, min_nprobes=config.min_nprobes, max_nprobes=config.max_nprobes, step_size=config.step_size)
+
+
+def _find_optimal_nr_probes(query_file: str, index_file: str, work_dir: str, gt: str, min_nprobes: int, max_nprobes: int, step_size: int):
+    # Load a pretrained Sentence Transformer model
+    model_name = "all-MiniLM-L6-v2"
+    model = SentenceTransformer(model_name)
+    logger.info(
+        f"Loaded model '{model_name}' (max sequence length = {model.max_seq_length}, embedding dimension = {model.get_sentence_embedding_dimension()})")
+
+    # Load the index from disk
+    index_name = os.path.basename(os.path.normpath(index_file))
+    index: faiss.Index = faiss.read_index(index_file)
+    logger.info(f"Loaded index from '{index_file}'.")
+
+    queries_df = pd.read_csv(query_file)
+    nr_queries = queries_df.shape[0]
+    # generate embeddings for queries
+    queries = queries_df["Query"].astype(str).tolist()
+    embeddings = model.encode(queries, batch_size=32, show_progress_bar=False, convert_to_numpy=True,
+                              precision="float32", normalize_embeddings=True)
+    ids = queries_df["Query number"].to_numpy()
+
+    # Load ground truth
+    ground_truth = pd.read_csv(gt)
+
+    nprobe_values = []
+    average_search_times = []
+    mean_precision_values = []
+    mean_recall_values = []
+
+    logger.info(
+        f"Initiating elbow method analysis: testing values for number of clusters probed from {min_nprobes} to {max_nprobes} with a step size of {step_size}.")
+
+    for n_probes in tqdm(range(min_nprobes, max_nprobes + 1, step_size), desc="Testing different values for number of clusters probed.",
+                      unit="test"):
+        # search the index with the generated embeddings
+        start_time = time.perf_counter()
+        _, nn_matrix = _query(index=index, query_embeddings=embeddings, k=10, n_probes=n_probes)
+        search_time_micros = (time.perf_counter() - start_time) * 1_000_000
+        average_search_time = search_time_micros / nr_queries
+
+        ids_repeated = np.repeat(ids, nn_matrix.shape[1])
+        nn_flat = nn_matrix.flatten()
+        df = pd.DataFrame({"Query_number": ids_repeated, "doc_number": nn_flat})
+        mean_precision, mean_recall = _compute_recall_and_precision(df, ground_truth)
+
+        nprobe_values.append(n_probes)
+        average_search_times.append(average_search_time)
+        mean_precision_values.append(mean_precision)
+        mean_recall_values.append(mean_recall)
+
+    _write_results(nprobe_values=nprobe_values, average_search_times=average_search_times,
+                   mean_precision_values=mean_precision_values, mean_recall_values=mean_recall_values,
+                   index_name=index_name, work_dir=work_dir)
+    _plot_results(nprobe_values=nprobe_values, average_search_time_values=average_search_times,
+                  mean_precision_values=mean_precision_values, mean_recall_values=mean_recall_values,
+                  index_name=index_name, work_dir=work_dir)
+
+
+def _query(index: faiss.Index, query_embeddings: npt.NDArray[np.float32], k: int, n_probes: int) -> Tuple[
+    npt.NDArray[np.float32], npt.NDArray[np.int64]]:
+    """
+    Search the index using the embeddings for the queries.
+    :param index: a faiss index instance, either of type IndexIDMap(IndexFlatIP) or of type IndexIVFFlat
+    :param query_embeddings: the embeddings for the queries
+    :param k: the number of neighbours to retrieve for each query
+    :param n_probes: the number of clusters to be probed during search (only affects indexes of type IndexIVFFlat)
+    :return: a tuple (D, I), where D will contain the distances and I will contain the indices of the neighbours
+    """
+    index = faiss.extract_index_ivf(index)
+    # number of clusters to visit
+    index.nprobe = n_probes
+    # search clusters
+    D, I = index.search(query_embeddings, k)
+    return D, I
+
+
+def _compute_recall_and_precision(rankings: pd.DataFrame, ground_truth: pd.DataFrame):
+    k = 10
+    # NOTE: order of documents within a group is preserved https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.groupby.html
+    # convert to dict (query_nr, set(doc_nr)), set is used since order DOES NOT matter
+    gt_dict = ground_truth.groupby("Query_number")["doc_number"].apply(set).to_dict()
+    # convert to dict (query_nr, list(doc_nr)), set is used since order DOES matter
+    rank_dict = rankings.groupby("Query_number")["doc_number"].apply(list).to_dict()
+
+    # check whether both dicts contain the same set of queries
+    assert (sorted(gt_dict.keys()) == sorted(gt_dict.keys()))
+
+    precisions = []
+    recalls = []
+
+    # calculate precision at k and recall at k for every query
+    for query, retrieved_docs in rank_dict.items():
+        # Get ground truth for the current query
+        relevant_docs = gt_dict.get(query, set())
+        # Limit retrieved docs to top k
+        top_k_docs = retrieved_docs[:k]
+        # Calculate intersection with relevant docs
+        retrieved_relevant_docs = set(top_k_docs) & relevant_docs
+
+        # Calculate precision@k
+        precision = len(retrieved_relevant_docs) / k
+        precisions.append(precision)
+
+        # Calculate recall@k
+        recall = len(retrieved_relevant_docs) / len(relevant_docs) if relevant_docs else 0
+        recalls.append(recall)
+
+    # Compute mean metrics for each k
+    mean_precision = sum(precisions) / len(precisions)
+    mean_recall = sum(recalls) / len(recalls)
+
+    return mean_precision, mean_recall
+
+
+def _write_results(nprobe_values: List, average_search_times: List, mean_precision_values: List,
+                   mean_recall_values: List,
+                   index_name: str,
+                   work_dir: str):
+    output_file = os.path.join(work_dir, f"{index_name}_nprobe_tuning_results.csv")
+    # Create a DataFrame
+    df = pd.DataFrame({
+        "nprobes": nprobe_values,
+        "average_search_time(microseconds)": average_search_times,
+        "mean_precision_at_10": mean_precision_values,
+        "mean_recall_at_10": mean_recall_values
+    })
+    df.to_csv(output_file, index=False, mode="w")
+    logger.info(f"Saved nprobes statistics to '{output_file}'.")
+
+
+def _plot_results(nprobe_values: List, average_search_time_values: List, mean_precision_values: List,
+                  mean_recall_values: List,
+                  index_name: str,
+                  work_dir: str):
+    # plot mean_precision_at_10
+    output_file_precision = os.path.join(work_dir, f"{index_name}_precision.png")
+    plt.plot(nprobe_values, mean_precision_values, 'bx-')
+    plt.xlabel('Number of probes (nprobes)')
+    plt.ylabel('Mean Precision at 10')
+    plt.title('Precision vs. Number of probes (nprobes)')
+    plt.grid()
+    plt.savefig(output_file_precision)
+    logger.info(f"Saved precision graph  to '{output_file_precision}'.")
+    plt.close()
+
+    # Plot average search times
+    output_file_search_time = os.path.join(work_dir, f"{index_name}_average_search_time.png")
+    plt.plot(nprobe_values, average_search_time_values, 'bx-')
+    plt.xlabel('Number of Probes (nprobes)')
+    plt.ylabel('Average Search Time (microseconds)')
+    plt.title('Average Search Time vs. Number of Probes (nprobes)')
+    plt.grid()
+    plt.savefig(output_file_search_time)
+    logger.info(f"Saved average search time graph to '{output_file_search_time}'.")
+    plt.close()
+
+    # Plot mean recall
+    output_file_recall = os.path.join(work_dir, f"{index_name}_mean_recall.png")
+    plt.plot(nprobe_values, mean_recall_values, 'bx-')
+    plt.xlabel('Number of Probes (nprobes)')
+    plt.ylabel('Mean Recall at 10')
+    plt.title('Mean Recall at 10 vs. Number of Probes (nprobes)')
+    plt.grid()
+    plt.savefig(output_file_recall)
+    logger.info(f"Saved mean recall graph to '{output_file_recall}'.")
+    plt.close()
